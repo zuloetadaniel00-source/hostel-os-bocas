e29c974b4af7117752e46d677fa17cb7d4ec841f
zuloetadaniel00-source <zuloetadaniel00@gmail.com>
2026-04-01 12:58:13 -0500

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+const supabaseUrl = 'https://tu-proyecto.supabase.co';
+const supabaseKey = 'tu-anon-key';
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
