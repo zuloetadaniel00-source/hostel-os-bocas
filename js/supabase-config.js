c0e861b3c71067c5eab4f626843fdea636028496
zuloetadaniel00-source <zuloetadaniel00@gmail.com>
2026-04-01 22:51:34 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 // supabase-config.js
-const supabaseUrl = 'https://tu-proyecto.supabase.co';
-const supabaseKey = 'tu-anon-key';
+const supabaseUrl = 'https://zgqzwiicunsckopmsrti.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpncXp3aWljdW5zY2tvcG1zcnRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5ODA0MDksImV4cCI6MjA5MDU1NjQwOX0.64EymmGBVG5glWZyaNDxM_bLVTz-x4d1zycHsaoC9pc';
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
